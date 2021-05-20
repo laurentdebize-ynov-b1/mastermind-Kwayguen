093f0a46efd830805c899f24e0a09eedab311aa8
hugo <hugo.roumagne@ynov.com>
2021-05-20 09:15:40 +0200

    mastermind mais sans l'algo final

--- fonctions.c
+++ fonctions.c
@@ -1,3 +1,6 @@
+#include <time.h>
+#include <stdlib.h>
+
 void affiche(int tab[], int NB_PIONS)
 {
     for (int i = 0; i < NB_PIONS; i++)
@@ -32,6 +35,49 @@ void lire(int tab[], int NB_PIONS, int NB_COULEURS)
                 tab[i] = number;
             } 
         } while (number < 0 || number > NB_COULEURS);
-        
+        number = -1;
+    }
+}
+
+int bienplace(int joueur[], int machine[], int nb_pions)
+{
+    int count;
+    for (int i = 0; i < nb_pions; i++)
+    {
+        if (joueur[i] == machine[i])
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int malplace(int joueur[],  int machine[], int nb_pions, int nb_couleurs)
+{
+    int malplaces = 0;
+    int cj = 0;
+    int cm = 0;
+
+    for (int i = 0; i < nb_couleurs; i++)
+    {
+        if (joueur[i] == i)
+        {
+            cj++;
+        }
+        if (machine[i] == i)
+        {
+            cm++;
+        }
+    }
+    malplaces += cj - cm;
+    return malplaces;
+}
+
+void init(int tab[], int nb_pions, int nb_couleurs)
+{
+    for (int i = 0; i < nb_pions; i++)
+    {
+        srand (time(NULL));
+        tab[i] = rand() % nb_couleurs;
     }
 }
